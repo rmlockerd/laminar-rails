2be7410d01edeb2f1c4b3c93c3476c7b02ccbd64
Robert Lockerd <rmlockerd@gmail.com>
2018-10-05 14:36:54 +1300

    Common generator

---
+++ lib/generators/common_generator.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# Generator for new particles.
+class CommonGenerator < Rails::Generators::NamedBase
+end
--- lib/generators/laminar/common_generator.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Laminar
-  # Generator for new particles.
-  class CommonGenerator < Rails::Generators::NamedBase
-  end
-end
